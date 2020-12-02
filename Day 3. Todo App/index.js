06f4861f692f27da41ff1f4c1b57dda46017435c
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-02 10:46:28 +0545

    TODO Update Fix array overwrite/reinitialize issue,to prevent old notes loss To fetch notes if notes are already exist in database else create empty array and push notes to array

--- Day 3. Todo App/index.js
+++ Day 3. Todo App/index.js
@@ -1,3 +1,5 @@
+
+
 //  when browser loads HTML completely
 // Do following
 window.onload = function(){
@@ -5,17 +7,37 @@ window.onload = function(){
 }
 
 let key ="my_notes"
+// let myNotes = []
+// To checks if notes already exists in storage
+// If notes already exist then fetch the notes
+// If notes are not exist create empty array
+// using ternary operator
+let myNotes = localStorage.getItem(key)?
+    JSON.parse(
+        localStorage.getItem(key)):[]
 // To add notes to local storage
 function onAddNotes(){
     // To get data from HTML we use dome
-    let myNotes =
+    let myNote =
     document.getElementById('notes').value
-    console.log(myNotes)
+    // To set text area null
+    document.getElementById('notes').value =''
+    // To store multiple values in notes
+    let note = {}
+    note.value = myNote
+    note.date = new Date()
+    // Gives unique timeframe for millisecond
+    note.id = new Date().getTime()
+    // console.log(myNote)
+    myNotes.push(note)
+    // When sending data to a web server, the data has to be a string.
+    // Convert a JavaScript object into a string with JSON.stringify().
+    // JSON : Array of objects
     // To store data to local storage
-    localStorage.setItem(key,myNotes)
+    localStorage.setItem(key,JSON.stringify(myNotes))
+    // // To store data to session
+    // sessionStorage.setItem(key,myNotes)
 
-    // To store data to session
-    sessionStorage.setItem(key,myNotes)
     // To get saved notes
     onGetSavedNotes()
 }
@@ -25,8 +47,28 @@ function onAddNotes(){
 function onGetSavedNotes(){
     // To get data from local storage using key
     let notes = localStorage.getItem(key)
-    // To write notes text to id
-    document.getElementById('myNotes').innerText = notes
-    console.log(notes)
+    // console.log(notes)
+    noteHTML = ''
+    JSON.parse(notes).forEach(function(val){
+        noteHTML = noteHTML +
+                `<div>
+                <p>${getReadableDate(val.date)}</p>
+                <p>${val.value}</p>
+                </div>`
+    })
 
+    // When receiving data from a web server, the data is always a string.
+    // Parse the data with JSON.parse(), and the data becomes a JavaScript object.
+    // To write notes text to id
+    document.getElementById('myNotes').innerHTML = noteHTML
+    // console.log(typeof(notes))
+    // console.log(notes)
+
+}
+
+
+function getReadableDate(date){
+    let dateReadable = new Date(date)
+    let fullDate = dateReadable.toDateString()
+    return fullDate
 }
