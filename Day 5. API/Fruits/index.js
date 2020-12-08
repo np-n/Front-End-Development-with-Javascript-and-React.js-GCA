678d94db2e9b05227c177fea35e31827b2f85a47
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-08 08:57:33 +0545

    Fetch data from API and display data to front end using promises

---
+++ Day 5. API/Fruits/index.js
@@ -0,0 +1,36 @@
+let API_URL = 'https://www.fruitmap.org/api/trees/'
+window.onload = function(){
+    onGetFruits()
+    // while loading data show loading status to user instead of blank screen
+    document.getElementById('fruits').innerHTML = `<div style="padding:30px;">loading data.....</div>`
+}
+function onGetFruits(){
+    // while loading data show loading status to user instead of blank screen
+    document.getElementById('fruits').innerHTML = `<div style="padding:30px;">loading data.....</div>`
+
+    fetch(API_URL,{
+        method:'GET',
+    }).
+    then(response => response.json()).
+    then(data => onDisplayData(data)).
+    catch(function(error){
+        document.getElementById('fruits').innerHTML = onGetErrorDiv()
+    }
+    )
+}
+function onDisplayData(fruits){
+    let fruits_html =''
+    fruits.forEach(function(value){
+        fruits_html += `<div style="border:2px solid ${value.color}"><b>${value.name}</b><br>
+                        ${value.count} </div>`
+    })
+
+    document.getElementById('fruits').innerHTML = fruits_html
+}
+
+function onGetErrorDiv(){
+    return `<div>
+    <div> Something Went Wrong. Please Try again!</div>
+    <button onclick ="onGetFruits()"> Try again</button>
+    </div>`
+}
