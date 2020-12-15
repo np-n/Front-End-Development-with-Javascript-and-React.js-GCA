c4afbcf1d52ac9a66168957d3ae6724de25d520c
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-15 08:25:19 +0545

    React app my-app-1 created in day 7

---
+++ Day 7. React Basics/my-app-1/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+// class based approach
+export default class App extends Component {
+  render() {
+    return(
+      <div>
+        Hello World from react...............
+        <>I am from Nepal</>
+      </div>
+    ) 
+  }
+}
+// export default App
