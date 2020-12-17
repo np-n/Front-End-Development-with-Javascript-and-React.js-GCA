7554294ab9f731bfd1550cbd9d9df6f1752e3693
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-17 14:55:20 +0545

    Digital clock completed
    componentDidMount() is called when first render is executed.
    We change our state in every seconds using the setInterval() function

--- Day 8/my-app-1/src/component/digitalcalculator.js
+++ Day 8/my-app-1/src/component/digitalcalculator.js
@@ -7,11 +7,26 @@ class DigitaCalculator extends Component{
           date: new Date()
         }
       }
+
+// will call when first render is executed
+componentDidMount(){
+    // To use this keyword further inside other function 
+    let self = this
+    // Call every second
+    this.state.timer = setInterval(function(){
+        // this function has this in scope 
+        // to refer state we use self variable which reference to state
+        self.setState({
+            date:new Date()
+        })
+
+    },1000)
+}
+
    render(){
        return(
 
         <div className ="headerName" style={{position:'relative', height:'150vh'}}>
-
             <div style={{display:'flex',position:'absolute',top:'25%',left:'40%'}}>
                 <div style={{padding:20}}>{this.state.date.getHours()}</div>
                 <div style={{padding:20}}>{this.state.date.getMinutes()}</div>
