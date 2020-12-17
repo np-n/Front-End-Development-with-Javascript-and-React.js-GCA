9986680527cff1150cfd8f9baa881fcd1d624354
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-17 10:09:40 +0545

    Alternative method of creating state instead of using constructor

--- Day 8/my-app-1/src/component/firstcomponent.js
+++ Day 8/my-app-1/src/component/firstcomponent.js
@@ -3,18 +3,18 @@ import {MySecondComponent} from './secondcomponent'
 
 class MyFirstComponent extends Component {
 
-    /* state={
-        username:"Netra"
-     } */
-    // state
-    constructor(props){
-        super(props)
-        this.state = {
-            username:""
-        }
-        //  // This binding is necessary to make `this` work in the callback
-        // this.handleChange = this.handleChange.bind(this)
-    }
+    state={
+        username:""
+     } 
+    // // state
+    // constructor(props){
+    //     super(props)
+    //     this.state = {
+    //         username:""
+    //     }
+    //     //  // This binding is necessary to make `this` work in the callback
+    //     // this.handleChange = this.handleChange.bind(this)
+    // }
 
     // to handle change
     // using arrow function to bind this
