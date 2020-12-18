190b3207390d406374663ec2321d2273a613c43f
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-18 14:44:06 +0545

    api call
    API call to http://coronavirus-19-api.herokuapp.com/countries
    and storing data from api to state and displaying countries to window

--- Day 8 (states,props,life cycle), Day 9/my-app-1/src/component/apiCall.js
+++ Day 8 (states,props,life cycle), Day 9/my-app-1/src/component/apiCall.js
@@ -1,10 +1,53 @@
 import React,{Component} from 'react'
-
+import axios from 'axios'
 class ApiCall extends Component{
+
+    // to store data let create a state
+    constructor(props){
+        super(props)
+        this.state ={
+            covidData:[],
+            // data is not loaded yet
+            loading:true
+        }
+    }
+
+    componentDidMount(){
+        this.getRemoteData()
+    }
+
+    getRemoteData= ()=>{
+        let self = this
+        axios.get('http://coronavirus-19-api.herokuapp.com/countries').
+        then(function(response){
+            // console.log(response)
+            // console.log(response.data)
+            self.setState({
+                covidData: response.data,
+                //data is already loaded, so it becomes false
+                loading:false
+            })
+        }).
+        catch(function(error){
+
+        })
+    }
     render(){
         return(
             <div>
-                This is API call
+                {// using ternery operator to check data loading status
+                this.state.loading ?
+                //if true: data is not loaded yet
+                <div>Data is loading...........</div>:
+                // if false:- data already loaded
+                <div>
+                    {
+                    this.state.covidData.map( (data)=>
+                    <div>{data.country}</div>
+                        )
+                    }
+                </div>
+                }
             </div>
         )
     }
