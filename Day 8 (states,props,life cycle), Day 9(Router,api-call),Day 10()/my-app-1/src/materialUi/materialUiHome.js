7d865a6328e7c87e5ab012a1be5a526502182fe5
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-20 10:41:54 +0545

    Grid Component
    The Material Design responsive layout grid adapts to screen size
     and orientation, ensuring consistency across layouts.
    
    sm -> for desktop
    xs -> for mobile
    total screen size is 12

--- Day 8 (states,props,life cycle), Day 9(Router,api-call),Day 10()/my-app-1/src/materialUi/materialUiHome.js
+++ Day 8 (states,props,life cycle), Day 9(Router,api-call),Day 10()/my-app-1/src/materialUi/materialUiHome.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import FormExample from './form'
 import Header from './header'
+import GridExample from './grid';
 
 
 class MaterialUIHome extends Component{
@@ -8,8 +9,8 @@ class MaterialUIHome extends Component{
         return(
             <div>
                 <Header/>
-                <div style={{padding:'25px 200px'}}>
-                <FormExample/>
+                <div style={{padding:'25px 20px'}}>
+                <GridExample/>
                 </div>
             </div>
         )
