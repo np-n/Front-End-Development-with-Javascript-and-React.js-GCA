6e0504ee9f20930421a4207aa6b1e4eb4fa34231
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-21 15:38:53 +0545

    Assignment 1 & 2 questions are added.

---
+++ Day 8 (states,props,life cycle), Day 9(Router,api-call),Day 10()/my-app-1/src/weatherInfo/weatherHome.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import {WeatherApi} from './weatherApi'
+import Header from './header'
+import {CircularProgress} from '@material-ui/core'
+import CurrentWeatherCard from './currentWeatherCard'
+import ErrorComponent from './errorComponent'
+export default class WeatherHome extends Component {
+    constructor(props){
+        super(props)
+            this.state ={
+                city:"pokhara",
+                weatherData:"",
+                isLoading:true,
+                error:false,
+            }
+        
+    }
+    componentDidMount(){
+        this.getWeatherData() 
+
+    }
+    getWeatherData =()=>{
+        self = this
+        WeatherApi.getCurrentWeatherData(self.state.city).
+        then(function(response){
+            // console.log(response.data)
+            self.setState({
+                weatherData:response.data,
+                isLoading:false
+
+            })
+            console.log(self.state.weatherData)
+        }).catch(function(error){
+            // console.log('Error')
+            self.setState({
+                error:true,
+            })
+        })   
+
+    }
+
+    onRetry =()=>{
+        this.setState({
+            error:false,
+            loading:true,
+        })
+        this.getWeatherData()
+    }
+    
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div style={{margin:'30px 8%'}}>
+                    {this.state.error?
+                    <ErrorComponent 
+                    onRetry ={this.onRetry}/>:
+                this.state.isLoading?
+                <div> Loading data...........             <CircularProgress/> 
+                        .......wait for a while</div>:
+                <div><CurrentWeatherCard
+                    data ={this.state.weatherData}/>
+                   </div>
+                 
+                }
+                </div> 
+            </div>
+                
+        )
+    }
+}
