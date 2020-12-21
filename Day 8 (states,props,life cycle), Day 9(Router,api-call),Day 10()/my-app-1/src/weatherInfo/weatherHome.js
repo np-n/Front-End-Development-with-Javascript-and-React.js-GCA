8b14745e06d020ec886765ca60ad8722cf2f8a97
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-21 13:48:26 +0545

    Weather api call and front end completed
    With this work day 11 is completed too.

--- Day 8 (states,props,life cycle), Day 9(Router,api-call),Day 10()/my-app-1/src/weatherInfo/weatherHome.js
+++ Day 8 (states,props,life cycle), Day 9(Router,api-call),Day 10()/my-app-1/src/weatherInfo/weatherHome.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {WeatherApi} from './weatherApi'
-
+import Header from './header'
+import {CircularProgress} from '@material-ui/core'
+import CurrentWeatherCard from './currentWeatherCard'
 export default class WeatherHome extends Component {
     constructor(props){
         super(props)
@@ -16,14 +18,38 @@ export default class WeatherHome extends Component {
 
     }
     getWeatherData =()=>{
-        WeatherApi.getCurrentWeatherData(this.state.city) 
+        self = this
+        WeatherApi.getCurrentWeatherData(self.state.city).
+        then(function(response){
+            // console.log(response.data)
+            self.setState({
+                weatherData:response.data,
+                isLoading:false
+
+            })
+            console.log(self.state.weatherData)
+        }).catch(function(error){
+            console.log('Error')
+        })
+           
+        
+        
 
     }
     render() {
         return (
             <div>
-            Weather
-                
+                <Header/>
+                <div style={{margin:'30px 8%'}}>
+                { this.state.isLoading?
+                <div> Loading data...........             <CircularProgress/> 
+                        .......wait for a while</div>:
+                <div><CurrentWeatherCard
+                    data ={this.state.weatherData}/>
+                   </div>
+                 }
+            
+                </div> 
             </div>
         )
     }
