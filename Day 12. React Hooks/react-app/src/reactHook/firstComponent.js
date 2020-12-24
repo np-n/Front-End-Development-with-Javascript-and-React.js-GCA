44ab7bd9d52d2324af7a2d64ff0a8f2c6a629993
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-24 09:11:03 +0545

    Toogle bulb in Light on-off example

--- Day 12. React Hooks/react-app/src/reactHook/firstComponent.js
+++ Day 12. React Hooks/react-app/src/reactHook/firstComponent.js
@@ -7,15 +7,27 @@ export default function FirstComponent() {
     // const [state, setstate] = useState(initialState)
     // Here 0 is default value/initialState
     const [counter, changeCounter] = useState(0)
+    const [isLightOn,setLightOnOff] = useState(false)
     const [imageUrl,setImageUrl] = useState('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.cKsIqa8C2cv7vezivnpo4wHaJ4%26pid%3DApi%26h%3D160&f=1')
  
+    function handleLight(){
+        if(isLightOn){
+            setImageUrl('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zHtYy800GhmmVF2UfkgymwHaJ4%26pid%3DApi%26h%3D160&f=1')
+        }
+        else{
+            setImageUrl('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.cKsIqa8C2cv7vezivnpo4wHaJ4%26pid%3DApi%26h%3D160&f=1')
+        }
+        // to toogle light on-off
+        setLightOnOff(!isLightOn)
+    }
+
     return (
         <div>
             <div>Counter value {counter}</div>
             <div><button 
             onClick={()=> changeCounter(counter+1)} >Increase counter</button></div>
             <img 
-            onClick={()=>setImageUrl('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.zHtYy800GhmmVF2UfkgymwHaJ4%26pid%3DApi%26h%3D160&f=1')}
+            onClick={()=>handleLight()}
             src={imageUrl}/>
         </div>
     )
