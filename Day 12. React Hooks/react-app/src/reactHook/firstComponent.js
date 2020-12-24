2e085b8a9d1fd771bf8a87ed653057f0313896b8
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-24 08:21:34 +0545

    Using useState hook at first time
    We call it inside a function component to add some local state to it.
    React will preserve this state between re-renders.
    useState returns a pair: the current state value and a function that
    lets you update it. You can call this function from an event handler or somewhere else. It’s similar to this.setState in a class, except it doesn’t merge the old and new state together.

--- Day 12. React Hooks/react-app/src/reactHook/firstComponent.js
+++ Day 12. React Hooks/react-app/src/reactHook/firstComponent.js
@@ -2,11 +2,15 @@ import React from 'react'
 import {useState} from 'react'
 
 export default function FirstComponent() {
-    
+    // setState is replaced by
+    // useState() in react hook
+    // const [state, setstate] = useState(initialState)
+    const [counter, changeCounter] = useState(0)
  
     return (
         <div>
-            I am from react functional component
+            <div>Counter value {counter}</div>
+            <div><button onClick={()=> changeCounter(counter+1)} >Increase counter</button></div>
         </div>
     )
 }
