77ae920069e6cfe8a7a69f19de18ead1f047ecd1
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-25 18:27:53 +0545

    useEffect() called everytime when iscalled state is set

--- Day 12. React Hooks/react-app/src/reactHook/useEffectHook.js
+++ Day 12. React Hooks/react-app/src/reactHook/useEffectHook.js
@@ -4,16 +4,17 @@ export default function UseEffectHook() {
     const [count,setCount] = useState(0)
     const [isCalled,setIsCalled] = useState(false)
 
-
+    // useEffect is called if isCalled state is set
     useEffect(()=>{
         console.log('This is useEffect hook')
-    })
+    },[isCalled])
 
     return (
         <div>
             <div>This is example of useEffect hook</div>
             <div>Count :{count}</div>
             <button onClick ={()=>setCount(count+1)}>Increase counter</button>
+            <button onClick ={()=>setIsCalled(true)}>setIsCalled</button>
 
         </div>
     )
