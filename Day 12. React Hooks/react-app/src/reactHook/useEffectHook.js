2fbd2e85a7f9c97323ddb6fa947af52b025eeb89
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-25 18:31:06 +0545

    useEffect() called only if either isCalled state changed or isCalledAgain state changed

--- Day 12. React Hooks/react-app/src/reactHook/useEffectHook.js
+++ Day 12. React Hooks/react-app/src/reactHook/useEffectHook.js
@@ -3,11 +3,12 @@ import React,{useEffect,useState} from 'react'
 export default function UseEffectHook() {
     const [count,setCount] = useState(0)
     const [isCalled,setIsCalled] = useState(false)
+    const [isCalledAgain,setIsCalledAgain] = useState(false)
 
     // useEffect is called if isCalled state is set
     useEffect(()=>{
         console.log('This is useEffect hook')
-    },[isCalled])
+    },[isCalled,isCalledAgain])
 
     return (
         <div>
@@ -15,6 +16,7 @@ export default function UseEffectHook() {
             <div>Count :{count}</div>
             <button onClick ={()=>setCount(count+1)}>Increase counter</button>
             <button onClick ={()=>setIsCalled(true)}>setIsCalled</button>
+            <button onClick ={()=>setIsCalledAgain(true)}>setIsCalledAgain</button>
 
         </div>
     )
