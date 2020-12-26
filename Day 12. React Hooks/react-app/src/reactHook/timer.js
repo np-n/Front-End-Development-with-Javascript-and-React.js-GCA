1cc642ee24deb43052c883d804e8a29d09db44eb
nimesh44 <nimesh.neupane43@gmail.com>
2020-12-26 09:41:25 +0545

    display timer in card and reset after timeout

--- Day 12. React Hooks/react-app/src/reactHook/timer.js
+++ Day 12. React Hooks/react-app/src/reactHook/timer.js
@@ -1,50 +1,64 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
+import { Card } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid';
 
 export default function Timer() {
-    const [time,setTime] = useState()
-    const [timer,setTimer] = useState()
-    const [isAlert,setIsAlert] = useState(false)
+  const [time, setTime] = useState();
+  const [timer, setTimer] = useState();
+  const [isAlert, setIsAlert] = useState(false);
+  let [secondCount,setSecondCount] = useState(0)
 
-    function handleChange(event){
-        console.log(event.target.value)
-        setTime(event.target.value)
-
-    }
-    function runTimer(){
-        let secondCount = 0
-        setTimer(setInterval(function(){
-            if(secondCount == time){
-                alert('Time Out!!')
-                setIsAlert(!isAlert)
-                secondCount = 0
-            }
-            else{
-                secondCount ++
-            }
-        },1000))
-
-    }
-    useEffect(()=>{
-        clearInterval(timer)
-    },[isAlert])
+  
+  function handleChange(event) {
+    console.log(event.target.value);
+    setTime(event.target.value);
+  }
+  function runTimer() {
+    setTimer(
+      setInterval(function () {
+        if (secondCount == time) {
+          alert("Time Out!!");
+          setIsAlert(!isAlert);
+          setSecondCount(0);
+          setTime(0)
+        } else {
+          setSecondCount(secondCount++);
+        }
+      }, 1000)
+    );
+  }
+  useEffect(() => {
+    clearInterval(timer);
+  }, [isAlert]);
   return (
-    <div style={{maxWidth:600,margin:'auto'}}>
-        <h2>This is timer app</h2>
-      <div><TextField
-        id="time"
-        onChange={handleChange}
-        value ={time}
-        label="Please enter your time"
-        helperText="Set timer in the basis of seconds"
-        fullWidth={true}
-        type ="number"
-        variant="filled"
-      />
-      </div>
-      <div>
-          <button style={{margin:'0 30%'}} onClick={()=>runTimer()}>Run Timer</button>
-      </div>
+    <div style={{ maxWidth: 600, margin: "auto" }}>
+      <h2>This is timer app</h2>
+      < Grid container  spacing={2}>
+            <Grid  item xs="8" sm="8">
+        <TextField
+          id="time"
+          onChange={handleChange}
+          value={time}
+          label="Please enter your time"
+          helperText="Set timer in the basis of seconds"
+          fullWidth={true}
+          type="number"
+          variant="filled"
+        />
+        </ Grid>
+        <Grid  item xs="4" sm="4">
+        <button style={{ marginTop:25}} onClick={() => runTimer()}>
+          Run Timer
+        </button>
+        </Grid>
+        </Grid>
+        <div>
+            <Card>
+                <h1 style={{ marginTop:30 ,textAlign:'center'}}>{secondCount}</h1>
+            </Card>
+        </div>
+     
     </div>
   );
 }
